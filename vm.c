f1c21cb05287dbedd6e068de1a37b964c4792f2b
Patrick Dorton <pdorton89@gmail.com>
2016-09-11 22:16:28 -0400

    vm.c file added
    Here is the main VM file to be edited 
    
    I will be pushing comments in here so that people can sign up for different sections of it.

---
+++ vm.c
@@ -0,0 +1,55 @@
+/*
+Team name: Group 30 aka Team Awesome 
+Patrick Dorton
+Andrew Maida
+David Almeida
+Michael Garro
+
+COP3402-16Fall001
+MW 7:30pm-8:45pm
+
+
+
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct instruction
+{
+	int op; /* opcode*/
+	int l;	// L
+	int m; 	// M
+} Instruction;
+
+Instruction Fetch(int pc);
+Void Execute(Instruction ir);
+
+
+
+
+int main()
+{
+	
+
+
+
+
+
+	return 0;
+}
+
+Instruction Fetch(int pc)
+{
+
+}
+
+
+
+Void Execute(Instruction ir)
+{
+
+}
+
+
