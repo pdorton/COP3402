43274cb9983869aad6ce1e743adb9910949b2a75
Patrick Dorton <pdorton89@gmail.com>
2016-09-19 14:34:56 -0400

    got it to compile

--- vm.c
+++ vm.c
@@ -35,9 +35,17 @@ int lineCount = 0;
 /*-------------------------User Defined Functions-----------------------------------*/
 
 void Fetch(Instruction ir, Instruction* instructionRegister);
-void Execute(FILE* output, Instruction* instructionRegister, int* haltFlag);
+void Execute(FILE* output, Instruction ir, int* haltFlag);
 void ReadInput(FILE* input, Instruction* intructions, int* lineCount);
+
 /*-------------------------End of Function Prototypes-------------------------------*/
+
+/*-------------------------------Unwritten functions--------------------------------*/
+void WriteInstructions(FILE* output, int lineCount,Instruction* instructions); // not yet defined
+void PrintStack(FILE* output, int Flag ,Instruction* instructions); // not yet defined
+int base(int l, int bp);
+
+/*-------------------------End of unwritten functions-------------------------------*/
 
 int main(int argc, const char* argv[])
 {
@@ -68,7 +76,8 @@ int main(int argc, const char* argv[])
 	// read in the input file and place into the array of instructions
 ReadInput(input, instructions, &lineCount);
 	
-	WriteInstructions(output, lineCount, instructions);
+	
+	//WriteInstructions(output, lineCount, instructions);
 	
 	fprintf(output, "STACK");
 	
@@ -78,12 +87,12 @@ ReadInput(input, instructions, &lineCount);
 		Fetch(instructions[pc], &instructionRegister);
 		//then increment the pc 
 		pc++;
-		Execute(output, &instructionRegister, &haltFlag);
+		Execute(output, instructionRegister, &haltFlag);
 		
 		if(instructionRegister.op == 5)
 		{// if a CAL function
 			calFlag = 1;
-			PrintStack(output, 0 , instructions);
+			//PrintStack(output, 0 , instructions);
 		}
 		else if(instructionRegister.op == 4 && instructionRegister.l > 0)
 		{
@@ -91,7 +100,7 @@ ReadInput(input, instructions, &lineCount);
 		}
 		else
 		{
-			PrintStack(output, calFlag, instructions);
+			//PrintStack(output, calFlag, instructions);
 		}
 		
 		fprintf(output, "\n");
@@ -212,10 +221,10 @@ void Execute(FILE* output, Instruction  ir, int* haltFlag)
     {
         case 01: //LIT
             sp = sp + 1;
-            stack[sp] = *ir.m;
+            stack[sp] = ir.m;
             break;
         case 02: //OPR
-            switch (*ir.m)
+            switch (ir.m)
             {
 
                 case 1: //NEG
@@ -285,29 +294,29 @@ void Execute(FILE* output, Instruction  ir, int* haltFlag)
 
         case 03:  //LOD
             sp = sp + 1;
-            stack[sp] = stack [base(*ir.l, bp) + ir.m];
+            stack[sp] = stack [ /*base(ir.l, bp)  +*/ ir.m];
             break;
 
         case 04:  //STO
-            stack[ base(*ir.l, bp) + *ir.m] = stack[sp];
+            stack[ /*base(ir.l, bp) +*/ ir.m] = stack[sp];
             sp = sp - 1;
             break;
 
         case 05: //CAL
             stack[sp + 1] = 0;                //return value (FV)
-            stack[sp + 2] = base(*ir.l, bp);   //static link (SL)
+            stack[sp + 2] = /*base(ir.l, bp)*/ 0;   //static link (SL)
             stack[sp + 3] = bp;               //dynamic link (DL)
             stack[sp + 4] = pc;               //return address (RA)
             bp = sp + 1;
-            pc = *ir.m;
+            pc = ir.m;
             break;
 
         case 06:  //INC
-            sp = sp + *ir.m;
+            sp = sp + ir.m;
             break;
 
         case 07:  //JMP
-            pc = *ir.m;
+            pc = ir.m;
             break;
     }
 }
