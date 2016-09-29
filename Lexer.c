22a5d0c94abdf005fc5f26bc77a071580216f89f
Michael Garro <mgarro385@knights.ucf.edu>
2016-09-28 20:07:32 -0400

    uisdbhisdvjk

---
+++ Lexer.c
@@ -0,0 +1,21 @@
+_/*
+COP 3402 Lexer Project
+Patrick Dorton
+Michael Garro
+David Almeida
+Andrew Maida
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+
+#define MAX_ID_LENGTH 12
+#define MAX_INTEGER 65535
+
+const char* const RESERVED_WORDS[] = {"const", "var", "procedure", 0};
+
+int main()
+{
+
+}
