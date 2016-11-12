9e504698a2a2301ca49fea143d03ed685c93acc3
Andrew Maida <agmaida@knights.ucf.edu>
2016-11-12 14:12:37 -0500

    added enums for tokens and opcodes

--- parser.c
+++ parser.c
@@ -38,6 +38,26 @@ typedef struct
 
 //
 
+enum token{nulsym = 1, identsym = 2, numbersym = 3, plussym = 4, minussym = 5, multsym = 6,
+slashsym = 7, oddsym = 8, eqlsym = 9, neqsym = 10, lessym = 11, leqsym = 12,
+gtrsym = 13, geqsym = 14, lparentsym = 15, rparentsym = 16, commasym = 17,
+semicolonsym = 18, periodsym = 19, becomessym = 20, beginsym = 21, endsym = 22,
+ifsym = 23, thensym = 24, whilesym = 25, dosym = 26, callsym = 27, constsym = 28,
+intsym = 29, procsym = 30, outsym = 31, insym = 32, elsesym = 33};
+
+enum opcode{
+  lit = 1,
+  opr = 2,
+  lod = 3,
+  sto = 4,
+  cal = 5,
+  inc = 6,
+  jmp = 7,
+  jpc = 8,
+  sio = 9,
+  sio2 = 10
+};
+
 TokenType tok;
 current_instruction instructions[MAX_CODE_SIZE];    // data structure for instructions and code generation
 symbol symbol_table[MAX_SYMBOL_TABLE_SIZE]; // currently here cause on slide, not syet used in program
